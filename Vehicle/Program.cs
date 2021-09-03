5009120874bfa15cd328dd4eff766208ae7eadbd
[ShubhamPV] <86839996+shubhamvadnere5@users.noreply.github.com>
2021-09-03 16:18:07 +0530

    master

---
+++ Vehicle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Car
+    {
+        string color = "Red";
+        int model = 3434;
+        
+        public void drive()
+        {
+            Console.WriteLine("Driving");
+            
+        }
+        public static void brake()
+        {
+            Console.WriteLine("new message added");
+        }
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.model);
+            myObj.drive();
+            brake();
+            Console.Read();
+        }
+    }
+}
